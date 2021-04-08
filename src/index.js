82fe6eed76684b0cb66e8af70bb211d81a73efa4
JakevK <jakevankeulen@gmail.com>
2021-04-09 08:34:35 +1200

    win detection

--- src/index.js
+++ src/index.js
@@ -16,23 +16,63 @@ class Board extends React.Component {
         [0, 0, 0, 0, 0, 0],
       ],
       turn: 0,
+      winner: 0,
     };
   }
 
+  checkForWin() {
+    const checkRow = (row) =>
+      new Set(row.slice(0, 5)).size === 1
+        ? row[0]
+        : row.length <= 5
+        ? 0
+        : checkRow(row.slice(1));
+
+    const squares = this.state.squares;
+
+    const checks = [
+      ...squares,
+      ...[...Array(6).keys()].map((n) => squares.map((row) => row[n])),
+      ...[squares, squares.slice(1), squares.map((x) => x.slice(1))].map((x) =>
+        x.map((row, i) => row[i])
+      ),
+      ...[
+        squares,
+        squares.slice(1),
+        squares.map((x) => x.slice(0, 5)),
+      ].map((x) => x.map((row, i) => row[row.length - 1 - i])),
+    ];
+
+    for (let i = 0; i < checks.length; i++) {
+      let winner = checkRow(checks[i]);
+      if (winner) return winner;
+    }
+
+    return 0;
+  }
+
   handleClickSquare(i, j) {
-    if (!this.state.squares[i][j]) {
+    if (!this.state.squares[i][j] && !this.state.winner) {
       let squaresTemp = this.state.squares;
       squaresTemp[i][j] = this.state.turn + 1;
+
       const newTurn = 1 - this.state.turn;
-      this.setState({ squares: squaresTemp, turn: newTurn });
-      this.props.handleTurnChange(newTurn);
+      const winner = this.checkForWin();
+
+      this.setState({
+        squares: squaresTemp,
+        turn: newTurn,
+        winner: winner,
+      });
+
+      this.props.handleTurnChange(newTurn, winner);
     }
   }
 
   render() {
     const board = this.state.squares.map((row, i) => {
       const squares = row.map((square, j) => {
-        const squareColor = ["white", "blue", "red"][square];
+        const squareColor = ["#efefef", "blue", "red"][square];
         const squareStyle = {
           backgroundColor: squareColor,
         };
@@ -42,9 +82,7 @@ class Board extends React.Component {
             className="board-square"
             key={j}
             onClick={() => this.handleClickSquare(i, j)}
-          >
-            {square}
-          </button>
+          ></button>
         );
       });
       return (
@@ -54,9 +92,17 @@ class Board extends React.Component {
       );
     });
     const boardStyle = {
-        border: `3px solid ${["blue", "red"][this.state.turn]}`
-    }
-    return <div className="board" style={boardStyle}>{board}</div>;
+      border: `3px solid ${
+        ["blue", "red"][
+          this.state.winner ? this.state.winner - 1 : this.state.turn
+        ]
+      }`,
+    };
+    return (
+      <div className="board" style={boardStyle}>
+        {board}
+      </div>
+    );
   }
 }
 
@@ -65,28 +111,45 @@ class Game extends React.Component {
     super(props);
     this.state = {
       turn: 0,
+      winner: 0,
     };
   }
-  handleTurnChange(turn) {
-    this.setState({ turn: turn });
+  handleTurnChange(turn, winner) {
+    this.setState({ turn: turn, winner: winner });
   }
   render() {
     const turnColor = ["blue", "red"][this.state.turn];
+    const winnerColor = ["blue", "red"][this.state.winner - 1];
     const turnColorStyle = {
       color: turnColor,
       fontWeight: "bold",
     };
+    const winnerColorStyle = {
+      color: winnerColor,
+      fontWeight: "bold",
+    };
+    const turnIndicator = this.state.winner ? (
+      <div className="turnIndicator">
+        <span style={winnerColorStyle}>{winnerColor}</span> won!
+      </div>
+    ) : (
+      <div className="turnIndicator">
+        <span style={turnColorStyle}>{turnColor}</span> player's turn
+      </div>
+    );
     return (
       <div className="container">
-        <div className="turnIndicator">
-          <span style={turnColorStyle}>{turnColor}</span> player's turn
-        </div>
-        <Board handleTurnChange={(turn) => this.handleTurnChange(turn)} />
+        {turnIndicator}
+        <Board
+          handleTurnChange={(turn, winner) =>
+            this.handleTurnChange(turn, winner)
+          }
+        />
       </div>
     );
   }
 }
 
-// ========================================
+//============================================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
